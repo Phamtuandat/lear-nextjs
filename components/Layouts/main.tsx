63416d5ba192dd3c0410f33c47e0496b63383c04
Pham Tuan Dat <phamtuandat1a0@gmail.com>
2021-12-15 08:56:44 +0700

    add material UI for next app

--- components/Layouts/main.tsx
+++ components/Layouts/main.tsx
@@ -1,4 +1,6 @@
 import { LayoutProp } from '@/models'
+import { Button } from '@mui/material'
+import { useAuth } from 'hooks'
 import Link from 'next/link'
 import * as React from 'react'
 import { Auth } from '../common'
@@ -6,17 +8,19 @@ import { Auth } from '../common'
 export interface MainLayoutProps {}
 
 export function MainLayout({ children }: LayoutProp) {
+    const { profile, isValidating, logout } = useAuth()
     return (
         <>
             <h1>Main Layout</h1>
             <Link href="/">
                 <a>Home</a>
             </Link>
-            <Auth>
+            {(profile as any)?.username ? (
                 <Link href="./dashboard">
                     <a>dashboard</a>
                 </Link>
-            </Auth>
+            ) : null}
+
             <div>{children}</div>
         </>
     )
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,39 @@
 import { EmptyLayout } from '@/components/Layouts'
+import { CacheProvider, ThemeProvider } from '@emotion/react'
+import { CssBaseline } from '@mui/material'
 import apiClient from 'axiosClients/apiClient'
+import Head from 'next/head'
 import { SWRConfig } from 'swr'
+import { createEmotionCache, theme } from 'Utils'
 import { AppPropsWithLayout } from '../models'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }: AppPropsWithLayout) {
+const clientSideEmotionCache = createEmotionCache()
+
+function MyApp({
+    Component,
+    pageProps,
+    emotionCache = clientSideEmotionCache,
+}: AppPropsWithLayout) {
     const Layout = Component.Layout ?? EmptyLayout
     return (
-        <SWRConfig value={{ fetcher: (url) => apiClient.get(url), shouldRetryOnError: false }}>
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </SWRConfig>
+        <CacheProvider value={emotionCache}>
+            <Head>
+                <title>My page</title>
+                <meta name="viewport" content="initial-scale=1, width=device-width" />
+            </Head>
+            <ThemeProvider theme={theme}>
+                {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+                <CssBaseline />
+                <SWRConfig
+                    value={{ fetcher: (url) => apiClient.get(url), shouldRetryOnError: false }}
+                >
+                    <Layout>
+                        <Component {...pageProps} />
+                    </Layout>
+                </SWRConfig>
+            </ThemeProvider>
+        </CacheProvider>
     )
 }
 
