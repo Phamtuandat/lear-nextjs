ac9c2aefa1309348b23bc31068f4257bd2c45a88
Pham Tuan Dat <phamtuandat1a0@gmail.com>
2022-01-07 23:18:53 +0700

    Dark mode for mobile

--- components/common/header/Header-mobile.tsx
+++ components/common/header/Header-mobile.tsx
@@ -1,25 +1,44 @@
 import CloseIcon from '@mui/icons-material/Close'
 import MenuIcon from '@mui/icons-material/Menu'
-import { Box, Drawer, IconButton, List, ListItem, Link as MuiLink, Divider } from '@mui/material'
+import {
+    Box,
+    Drawer,
+    IconButton,
+    List,
+    ListItem,
+    Link as MuiLink,
+    Divider,
+    useTheme,
+} from '@mui/material'
 import Link from 'next/link'
-import React from 'react'
+import React, { useContext } from 'react'
 import { ROUTE_LIST } from './router'
 import clsx from 'clsx'
 import { useRouter } from 'next/router'
+
+import Brightness4Icon from '@mui/icons-material/Brightness4'
+import Brightness7Icon from '@mui/icons-material/Brightness7'
+import { ColorModeContext } from 'Utils/themeContext'
 
 export interface HeaderMobileProp {}
 
 export function HeaderMobile(props: HeaderMobileProp) {
     const [open, setOpen] = React.useState(false)
+    const theme = useTheme()
+    const colorMode = useContext(ColorModeContext)
     const handleClose = () => {
         setOpen(false)
     }
     const router = useRouter()
     return (
         <Box display={{ xs: 'flex', md: 'none' }} py={2} justifyContent={'flex-end'}>
+            <IconButton sx={{ ml: 1 }} onClick={colorMode.toggleColorMode} color="inherit">
+                {theme.palette.mode === 'dark' ? <Brightness7Icon /> : <Brightness4Icon />}
+            </IconButton>
             <IconButton onClick={() => setOpen(true)}>
                 <MenuIcon sx={{ fontSize: '36px' }} />
             </IconButton>
+
             <Drawer anchor="right" open={open} onClose={handleClose}>
                 <Box sx={{ position: 'relative' }}>
                     <List sx={{ width: '275px', py: 4 }}>
